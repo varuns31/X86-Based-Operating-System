88d939a2f33ae7f0b2687fe25b7d2a53a47a8ab2
Gupta <sg49@illinois.edu>
2022-10-17 13:18:50 -0500

    keyboard works

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,14 +1,27 @@
 #include "keyboard.h"
 
+char key_map[50] = {
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '-', '\b',  
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
+    '~', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '\r',
+    '~', '~', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '~'
+};
+
 void keyboard_handler_init() {
     enable_irq(1);
+    clear();
     return;
 }
 
 void keyboard_handler() {
-    clear();
-    int a = inb(0x60);
-    printf(" The key pressed is %d\n", a);
+    int scan_code = inb(0x60);
+    if(a > 0x4d) {
+        send_eoi(0x01);
+        return;
+    }
+
+    putc(key_map[scan_code - 0x02]);
     send_eoi(0x01);
+
     return;
 }
