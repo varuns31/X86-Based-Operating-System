1c15a56ed8a2d686cb0ea7867dc0dce0f3e4c9b1
Gupta <sg49@illinois.edu>
2022-10-17 16:12:57 -0500

    final rtc

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -1,26 +1,49 @@
 #include "rtc.h"
 
+#define IRQ_LINE_RTC 0x08
+#define RTC_PORT_ADDR 0x70
+#define RTC_PORT_DATA 0x71
+
+#define REG_A_IDX 0x8A
+#define REG_B_IDX 0x8B
+#define REG_C_IDX 0x8C
+
 void rtc_handler_init() {
-    char prev;
+    char init_val;
     unsigned int rate;
 
-    enable_irq(0x08);
+    enable_irq(IRQ_LINE_RTC);
+
+    // Register B setup
+    // setup register B
+    outb(REG_B_IDX, RTC_PORT_ADDR);
+
+    // get value of register B
+    init_val = inb(RTC_PORT_DATA);
+
+    // set register B index again
+    outb(REG_B_IDX, RTC_PORT_ADDR);
+
+    // turn on bit 6 (0x40) of register B 
+    outb(init_val | 0x40, RTC_PORT_DATA);
 
 
-    // Register B
-    outb( 0x8B, 0x70);		    // select register B, and disable NMI
-    prev = inb(0x71);	    // read the current value of register B
-    outb( 0x8B, 0x70);		    // set the index again (a read will reset the index to register D)
-    outb( prev | 0x40, 0x71);	// write the previous value ORed with 0x40. This turns on bit 6 of register B
-    
+    // rate is 1024 (2^(10-4))
+    rate = 6;
 
-    rate = 6;			// rate is 1024
 
-    // Register A
-    outb( 0x8A, 0x70);		// set index to register A, disable NMI
-    prev = inb(0x71);	// get initial value of register A
-    outb( 0x8A, 0x70);		// reset index to A
-    outb( (prev & 0xF0) | rate, 0x71); //write only our rate to A. Note, rate is the bottom 4 bits.
+    // Register A setup
+    // set register A index again
+    outb(REG_A_IDX, RTC_PORT_ADDR);
+
+    // get value of register A
+    init_val = inb(RTC_PORT_DATA);	
+
+    // set register A index again
+    outb(REG_A_IDX, RTC_PORT_ADDR);		
+
+    // set botton 4 bits to rate
+    outb((init_val & 0xF0) | rate, RTC_PORT_DATA); 
 
     return; 
 }
@@ -28,9 +51,16 @@ void rtc_handler_init() {
 void rtc_handler() {
 
     // Register C
-    outb( 0x0C, 0x70);	// select register C
-    inb(0x71);		// just throw away contents
+    // setup register C
+    outb(REG_C_IDX, RTC_PORT_ADDR);
+    
+    // flush the current value
+    inb(RTC_PORT_DATA);
+
+    // call test case
     test_interrupts();
-    send_eoi(0x08);
+    
+    send_eoi(IRQ_LINE_RTC);
+
     return; 
 }
