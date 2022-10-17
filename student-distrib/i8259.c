111bd884716d62bdcbd08e4e81bf6d98ae015eb7
Gupta <sg49@illinois.edu>
2022-10-17 00:51:39 -0500

    pic compiles idk if works

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -11,16 +11,73 @@ uint8_t slave_mask;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
+
+    // init cached values
+    master_mask = 0xff;
+    slave_mask = 0xff;
+
+    // commands for master
+    outb(ICW1, MASTER_8259_PORT);
+    outb(ICW2_MASTER, MASTER_8259_PORT + 0x01);
+    outb(ICW3_MASTER, MASTER_8259_PORT + 0x01);
+    outb(ICW4, MASTER_8259_PORT + 0x01);
+
+    // commands for slave
+    outb(ICW1, SLAVE_8259_PORT);
+    outb(ICW2_SLAVE, SLAVE_8259_PORT + 0x01);
+    outb(ICW3_SLAVE, SLAVE_8259_PORT + 0x01);
+    outb(ICW4, SLAVE_8259_PORT + 0x01);
+
+    // push cached values
+    outb(master_mask, MASTER_8259_PORT + 0x01);
+    outb(slave_mask, SLAVE_8259_PORT + 0x01);
+
+    return;
 }
 
 /* Enable (unmask) the specified IRQ */
 void enable_irq(uint32_t irq_num) {
+
+    if(irq_num < 8) {
+        master_mask &= ~(0x01 << irq_num);
+        outb(master_mask, MASTER_8259_PORT + 0x01); 
+        return;
+    }
+
+    irq_num -= 8;
+    slave_mask &= ~(0x01 << irq_num);
+    outb(slave_mask, SLAVE_8259_PORT + 0x01);
+    
+    return;
 }
 
 /* Disable (mask) the specified IRQ */
 void disable_irq(uint32_t irq_num) {
+    
+    if(irq_num < 8) {
+        master_mask |= 0x01 << irq_num;
+        outb(master_mask, MASTER_8259_PORT + 0x01); 
+        return;
+    }
+
+    irq_num -= 8;
+    slave_mask |= 0x01 << irq_num;
+    outb(slave_mask, SLAVE_8259_PORT + 0x01);
+    
+    return;
 }
 
 /* Send end-of-interrupt signal for the specified IRQ */
 void send_eoi(uint32_t irq_num) {
+
+    if(irq_num < 8) {
+        outb(EOI | irq_num, MASTER_8259_PORT);
+        return;
+    }
+
+    irq_num -= 8;
+    outb(EOI | 2, MASTER_8259_PORT);
+    outb(EOI | irq_num, SLAVE_8259_PORT);
+
+    return;
 }
