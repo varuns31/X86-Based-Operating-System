8bf80840abcd709593f50f81e5e9bb1198bfd21d
Singhal <varuns7@illinois.edu>
2022-10-20 23:47:27 -0500

    Buffer created for keyboard

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -26,7 +26,15 @@ struct keyboard_mapper {
     int alt;
 } key_tracker;
 
-int cur_line_counter = 1;
+typedef struct keyboard_buffer{
+    char *line;
+    int length;
+};
+
+struct keyboard_buffer onscreen_buff[25]; 
+int buf_length;
+
+int cur_line_counter = 0;
 
 int is_alpha(int scan_code);
 
@@ -56,6 +64,14 @@ void keyboard_handler_init() {
     key_tracker.shift = 0;
     key_tracker.ctrl = 0;
     key_tracker.alt = 0;
+    buf_length=0;
+    int i=0;
+    for(i=0;i<25;i++)
+    {
+        char init_line[128];
+        onscreen_buff[i].line=init_line;
+        onscreen_buff[i].length=0;
+    }
     
     return; 
 }
@@ -120,6 +136,7 @@ void keyboard_handler() {
     if(scan_code==28)
     {
         cur_line_counter++;
+        buf_length++;
     }
 
     char to_print;
@@ -127,13 +144,56 @@ void keyboard_handler() {
     if(!is_alpha(scan_code))caps_idx=key_tracker.shift;
     to_print=key_map[caps_idx][scan_code];
 
+    // if (onscreen_buff[buf_length].length==80)
+    // {
+    //     putc('\n');
+    //     cur_line_counter++;
+    // }
+
     if(cur_line_counter >= 25)
     {
+        int a,b;
         clear();
+        // for(a=0;a<onscreen_buff.length();a++)
+        // {
+        //     onscreen_buff[a].line=onscreen_buff[a+1].line;
+        //     for(b=0;b<onscreen_buff[a+1].length;b++)
+        //     {
+        //         putc(onscreen_buff[a+1].line[b]);
+        //     }
+        // }
+        printf("%d",onscreen_buff[0].length);
         cur_line_counter = 0;
+        buf_length=0;
     }
     // write character to the screen
-    putc(to_print);
+
+    if(scan_code==14)
+    {
+        int spaces=160-onscreen_buff[buf_length].length;
+        int counter=0;
+        for(counter=0;counter<spaces;counter++)
+        {
+            putc(' ');
+        }
+        for(counter=0;counter<onscreen_buff[buf_length].length-1;counter++)
+        {
+            printf("%f",onscreen_buff[buf_length].line[counter]);
+        }
+        onscreen_buff[buf_length].length-=1;
+        send_eoi(IRQ_LINE_KEYBOARD); 
+        return;
+    }
+   
+
+    if(onscreen_buff[buf_length].length<128)
+    {
+        onscreen_buff[buf_length].line[onscreen_buff[buf_length].length]=to_print;
+        onscreen_buff[buf_length].length+=1;
+        //printf("%d\n",scan_code);
+        putc(to_print);
+        //printf("\n%c",to_print);
+    }
 
     send_eoi(IRQ_LINE_KEYBOARD); 
 
