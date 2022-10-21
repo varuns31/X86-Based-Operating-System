21d2fec8e5dc4af8293d1a3c7b7cefc6a72d05f0
Bala <ambala2@illinois.edu>
2022-10-20 23:41:39 -0500

    rtc in progress

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -23,6 +23,13 @@
  *   RETURN VALUE: none
  *   SIDE EFFECTS: clobbers ports for device
  */
+
+ // register A - rate divider rule
+ // register B - disbale NMI (non maskable interrupts)
+ // register C - upon a IRQ 8, Status Register C will contain a bitmask telling which interrupt happened.
+ //            - What is important is that if register C is not read after an IRQ 8, then the interrupt 
+ //            - will not happen again. So, even if you don't care about what type of interrupt it is, 
+ //            - just attach this code to the bottom of your IRQ handler to be sure you get another interrupt.
 void rtc_handler_init() {
     char init_val;
     unsigned int rate;
@@ -63,13 +70,34 @@ void rtc_handler_init() {
     return; 
 }
 
+
+void rtc_handler_set_rate(unsigned int rate) {
+    char init_val;
+
+    // Register A setup
+    // set register A index again
+    outb(REG_A_IDX, RTC_PORT_ADDR);
+
+    // get value of register A
+    init_val = inb(RTC_PORT_DATA);	
+
+    // set register A index again
+    outb(REG_A_IDX, RTC_PORT_ADDR);		
+
+    // set botton 4 bits to rate
+    outb((init_val & 0xF0) | rate, RTC_PORT_DATA); 
+
+    return; 
+}
+
+
 /* 
  * rtc_handler
  *   DESCRIPTION: interrupt handler for RTC
  *   INPUTS: none
  *   OUTPUTS: none
  *   RETURN VALUE: none
- *   SIDE EFFECTS: clobbers ports for device
+ *   SIDE EFFECTS: 
  */
 void rtc_handler() {
 
@@ -87,3 +115,84 @@ void rtc_handler() {
 
     return; 
 }
+
+
+volatile unsigned int rtc_flag = 0; 
+
+/* 
+ * rtc_read
+ *   DESCRIPTION: 
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: 
+ */
+int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes){
+    while(rtc_flag);
+    return 0;
+}
+
+/* 
+ * rtc_write
+ *   DESCRIPTION: 
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: 
+ */
+int32_t rtc_write (int32_t fd, const void* buf, int32_t nbytes){
+
+    // check if buffer it 4 bytes
+    if(sizeof(buf) != sizeof(uint32_t))
+        return -1;
+
+    // get frequence from the buffer
+    unsigned int frequency = *((unsigned int*)buf);
+    
+    // check if frequency within boounds and a power of 2
+    if(frequency > 1024 || frequency < 2  || frequency % 2 != 0)
+        return -1;
+
+    // The formula to calculate the interrupt rate is:
+    //   freq. = 32768 >> (rate-1)
+    unsigned int rate = 1;
+
+    unsigned int freq_count = 32768;
+
+    while(freq_count != frequency){
+        rate++;
+        freq_count >>= 1;
+    }
+
+    rtc_handler_set_rate(rate);
+
+    return 0;
+}
+
+
+/* 
+ * rtc_open
+ *   DESCRIPTION: 
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: 
+ */
+int32_t rtc_open (const uint8_t* filename){
+    if(filename == NULL)
+        return -1;
+    return 0;
+}
+
+/* 
+ * rtc_close
+ *   DESCRIPTION: 
+ *   INPUTS: none
+ *   OUTPUTS: none
+ *   RETURN VALUE: none
+ *   SIDE EFFECTS: 
+ */
+int32_t close (int32_t fd){
+    return 0;
+}
+
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -1,5 +1,17 @@
 #include "i8259.h"
 #include "lib.h"
 
+
+
 void rtc_handler_init();
 void rtc_handler();
+
+void rtc_handler_set_rate(unsigned int rate);
+
+int32_t rtc_read (int32_t fd, void* buf, int32_t nbytes);
+
+int32_t rtc_write (int32_t fd, const void* buf, int32_t nbytes);
+
+int32_t rtc_open (const uint8_t* filename);
+
+int32_t rtc_close (int32_t fd);
