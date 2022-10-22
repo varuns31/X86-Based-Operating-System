8effef36455be43588b9f944b3320e9a66862e7b
Singhal <varuns7@illinois.edu>
2022-10-22 18:12:05 -0500

    File system initialises

--- student-distrib/fs.c
+++ student-distrib/fs.c
@@ -1,1 +1,18 @@
 #include "fs.h"
+
+
+
+struct boot_block our_boot_block;
+struct inode our_inodes[NUM_POSSIBLE_ENTRIES];
+struct data_block our_data_blocks[NUM_DATABLOCK_ENTRIES*NUM_POSSIBLE_ENTRIES];
+void init_file_driver()
+{
+    our_boot_block.num_dir_entries = 0;
+    our_boot_block.num_inodes = 0;
+    our_boot_block.num_datablocks = 0;
+    
+    our_boot_block.dir_entries[0].file_name[0] = '.';
+    our_boot_block.dir_entries[0].file_type = 1;
+    our_boot_block.dir_entries[0].inode_number = 0;
+
+}
--- student-distrib/fs.h
+++ student-distrib/fs.h
@@ -6,7 +6,7 @@
 #define DATABLOCK_SIZE 4096 //4kb data blocks
 #define MAX_BYTES_IN_BLOCK (DATABLOCK_SIZE/4) //each entry is 4bytes
 #define NUM_POSSIBLE_ENTRIES (DATABLOCK_SIZE/64)-1 //each directory entry is 64bytes
-
+#define NUM_DATABLOCK_ENTRIES MAX_BYTES_IN_BLOCK-1 // First value in inode is length rest are datablocks
 
 typedef struct dentry_object 
 {
@@ -28,12 +28,12 @@ typedef struct boot_block
 typedef struct inode 
 {
     uint32_t length;
-    uint32_t data_block_number[MAX_BYTES_IN_BLOCK-1];
+    uint32_t data_block_number[NUM_DATABLOCK_ENTRIES];
 } __attribute__ ((packed));
 
 typedef struct data_block 
 {
-    uint32_t data_block[MAX_BYTES_IN_BLOCK];
+    uint32_t data_blocks[MAX_BYTES_IN_BLOCK];
 } __attribute__ ((packed));
  
 #endif /* FS_H */
