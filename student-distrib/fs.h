52d9ed7d600452438dfb6c6cfac82c840f867b10
Singhal <varuns7@illinois.edu>
2022-10-22 17:37:27 -0500

    File system data structures defined

--- student-distrib/fs.h
+++ student-distrib/fs.h
@@ -3,12 +3,17 @@
 
 #include "lib.h"
 
+#define DATABLOCK_SIZE 4096 //4kb data blocks
+#define MAX_BYTES_IN_BLOCK (DATABLOCK_SIZE/4) //each entry is 4bytes
+#define NUM_POSSIBLE_ENTRIES (DATABLOCK_SIZE/64)-1 //each directory entry is 64bytes
+
+
 typedef struct dentry_object 
 {
-    char file_name[32];
+    char file_name[32];//32 Byte file size
     uint32_t file_type;
     uint32_t inode_number;
-    char reserved[24];
+    char reserved[24];//24 Bytes reserved in dentry
 } __attribute__ ((packed));
 
 typedef struct boot_block 
@@ -16,14 +21,19 @@ typedef struct boot_block
     uint32_t num_dir_entries;
     uint32_t num_inodes;       
     uint32_t num_datablocks;
-    char reserved[52];
-    struct dentry_object dir_entries[63];
+    char reserved[52];//52 Bytes reserved in boot_block
+    struct dentry_object dir_entries[NUM_POSSIBLE_ENTRIES];
 } __attribute__ ((packed));
 
 typedef struct inode 
 {
     uint32_t length;
-    uint32_t data_block_number[1023];
+    uint32_t data_block_number[MAX_BYTES_IN_BLOCK-1];
+} __attribute__ ((packed));
+
+typedef struct data_block 
+{
+    uint32_t data_block[MAX_BYTES_IN_BLOCK];
 } __attribute__ ((packed));
  
 #endif /* FS_H */
